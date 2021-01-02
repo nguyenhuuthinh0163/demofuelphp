14b78b821886a69ba13b94bf11861a4370f4d4e1
thinhdactech <thinh@dactech.com>
2021-01-02 18:21:48 +0700

    second commit

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Hi, I'm FuelPHP Framework . Nice to meet you</title>
+	<title>Hi, I'm FuelPHP Framework . Nice to meet you
+	this is second commit</title>
 	<?php echo Asset::css('bootstrap.css'); ?>
 	<style>
 		#logo{
